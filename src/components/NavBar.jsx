c7fd1ea681de98d5748d7918ddcb54f201a958b2
alexmargomez <fabioalexmar@gmail.com>
2025-01-30 12:20:37 -0500

    Add details to the Dashboard and update routes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,6 @@ import { HiOutlineDocumentMagnifyingGlass } from "react-icons/hi2";
 import { FaDoorClosed, FaDoorOpen } from "react-icons/fa";
 import { Logout } from './LoginVal';
 import { MdDashboard, MdOutlineDashboard, MdOutlinePointOfSale } from "react-icons/md";
-import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
 
 
 
@@ -23,24 +22,25 @@ const NavBar = () => {
   const [isReportsOpen, setIsReportsOpen] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
-  const handleLogout = Logout(navigate) 
-  
-  const handleClick = () => {
-    navigate('/dashboard');
-  };
-  const handleClick2 = () => {
-    navigate('/look');
-  };
 
-  
+  const [hoveredIcon, setHoveredIcon] = useState(null);
 
+  const Icons = [
+    { icon: HiOutlineDocumentMagnifyingGlass, route: "/look" },
+    { icon: MdOutlinePointOfSale, route: "/sales" },
+    { icon: AiFillProduct, route: "/inventory" },
+    { icon: FaUsersRays, route: "/clients" },
+    { icon: PiMotorcycleFill, route: "/vehicles" },
+    { icon: HiDocumentReport, route: "/reports" },
+  ];
+  
   return (
     <div className="bg-[#494A8A]  h-screen w-1/17 flex flex-col">
         <div 
-          className='flex flex-col h-2/10 items-center justify-center'
+          className='flex flex-col h-1/10 items-center justify-center'
           onMouseEnter={() => setIsDashboardOpen(true)}
           onMouseLeave={() => setIsDashboardOpen(false)}
-          onClick= {handleClick}
+          onClick={ () => navigate("/dashboard")}
         >    
           {isDashboardOpen ? (
             <MdDashboard className="text-white text-3xl" /> 
@@ -50,101 +50,31 @@ const NavBar = () => {
           )}
         </div>
 
-        <div className="flex flex-col items-center justify-center space-y-10 h-9/10">
-
-          {/* Look Icon */}
-          <div
-            onMouseEnter={() => setIsLookOpen(true)}
-            onMouseLeave={() => setIsLookOpen(false)}
-            onClick={handleClick2}
-          >
-            <div className={`transition-all duration-100 transform p-1 rounded-full ${isLookOpen ? 'bg-yellow-500 border-2 border-yellow-500' : 'bg-transparent'}`}>
-              {isLookOpen ? (
-                <HiOutlineClipboardDocumentList className="text-white text-2xl" />
-              ) : (
-                <HiOutlineDocumentMagnifyingGlass className="text-white text-2xl" />
-              )}
+        <div className="flex flex-col items-center justify-center space-y-8 flex-grow h-8/10">
+          {Icons.map((item, index) => (
+            <div
+              key={index}
+              onMouseEnter={() => setHoveredIcon(index)}
+              onMouseLeave={() => setHoveredIcon(null)}
+              onClick={() => navigate(item.route)}
+              className="cursor-pointer"
+            >
+              <div
+                className={`transition-all duration-100 p-2 rounded-full ${
+                  hoveredIcon === index ? 'bg-[#FFD700] border-2 border-[#FFD700]' : 'bg-transparent'
+                }`}
+              >
+                <item.icon className="text-white text-2xl" />
+              </div>
             </div>
-          </div>
-
-          {/* Sales Icon */}
-          <div
-            onMouseEnter={() => setIsSalesOpen(true)}
-            onMouseLeave={() => setIsSalesOpen(false)}
-          >
-            <div className={`transition-all duration-100 transform p-1 rounded-full ${isSalesOpen ? 'bg-yellow-500 border-2 border-yellow-500' : 'bg-transparent'}`}>
-              {isSalesOpen ? (
-                <MdOutlinePointOfSale className="text-white text-2xl" />
-              ) : (
-                <MdOutlinePointOfSale className="text-white text-2xl" /> // El mismo icono para el estado "no hover"
-              )}
-            </div>
-          </div>
-
-          {/* Inventory Icon */}
-          <div
-            onMouseEnter={() => setIsInventoryOpen(true)}
-            onMouseLeave={() => setIsInventoryOpen(false)}
-          >
-            <div className={`transition-all duration-100 transform p-1 rounded-full ${isInventoryOpen ? 'bg-yellow-500 border-2 border-yellow-500' : 'bg-transparent'}`}>
-              {isInventoryOpen ? (
-                <AiFillProduct className="text-white text-2xl" />
-              ) : (
-                <AiFillProduct className="text-white text-2xl" /> // El mismo icono para el estado "no hover"
-              )}
-            </div>
-          </div>
-
-          {/* Clients Icon */}
-          <div
-            onMouseEnter={() => setIsClientsOpen(true)}
-            onMouseLeave={() => setIsClientsOpen(false)}
-          >
-            <div className={`transition-all duration-100 transform p-1 rounded-full ${isClientsOpen ? 'bg-yellow-500 border-2 border-yellow-500' : 'bg-transparent'}`}>
-              {isClientsOpen ? (
-                <FaUsersRays className="text-white text-2xl" />
-              ) : (
-                <FaUsersRays className="text-white text-2l" /> // El mismo icono para el estado "no hover"
-              )}
-            </div>
-          </div>
-
-          {/* Vehicles Icon */}
-          <div
-            onMouseEnter={() => setIsVehiclesOpen(true)}
-            onMouseLeave={() => setIsVehiclesOpen(false)}
-          >
-            <div className={`transition-all duration-100 transform p-1 rounded-full ${isVehiclesOpen ? 'bg-yellow-500 border-2 border-yellow-500' : 'bg-transparent'}`}>
-              {isVehiclesOpen ? (
-                <PiMotorcycleFill className="text-white text-2xl" />
-              ) : (
-                <PiMotorcycleFill className="text-white text-2xl" /> // El mismo icono para el estado "no hover"
-              )}
-            </div>
-          </div>
-
-          {/* Reports Icon */}
-          <div
-            onMouseEnter={() => setIsReportsOpen(true)}
-            onMouseLeave={() => setIsReportsOpen(false)}
-          >
-            <div className={`transition-all duration-100 transform p-1 rounded-full ${isReportsOpen ? 'bg-yellow-500 border-2 border-yellow-500' : 'bg-transparent'}`}>
-              {isReportsOpen ? (
-                <HiDocumentReport className="text-white text-2xl" />
-              ) : (
-                <HiDocumentReport className="text-white text-2xl" /> // El mismo icono para el estado "no hover"
-              )}
-            </div>
-          </div>
-
+          ))}
         </div>
-
-
+          
         <div
-          className="flex flex-col h-2/10 items-center justify-center transition-all duration-300"
+          className="flex flex-col h-1/10 items-center justify-center transition-all duration-300"
           onMouseEnter={() => setIsOpen(true)}
           onMouseLeave={() => setIsOpen(false)}
-          onClick={handleLogout}
+          onClick={Logout(navigate)}
         >
           {isOpen ? (
             <FaDoorOpen className="text-white text-2xl transition-all duration-300" />
--- src/screens/dashboard.jsx
+++ src/screens/dashboard.jsx
@@ -5,15 +5,17 @@ import { MdShoppingCartCheckout } from "react-icons/md";
 import { TbReportAnalytics } from "react-icons/tb";
 import { PiMotorcycleFill } from "react-icons/pi";
 import DateTimeDisplay from '../components/DateTimeDisplay';
+import { useNavigate } from 'react-router-dom';
 
 const dashboard = () => {
+  const navigate = useNavigate();
   const blocks = [
-    { bgColor: "#C05E91", number: "1", name: "Facturas", icon: <FaFileInvoiceDollar /> },
-    { bgColor: "#FFC39D", number: "2", name: "Inventario", icon: <MdOutlineInventory /> },
-    { bgColor: "#F86888", number: "4", name: "Clientes", icon: <FaUsersViewfinder /> },
-    { bgColor: "#FFC39D", number: "3", name: "Vehiculos", icon: <PiMotorcycleFill /> },
-    { bgColor: "#925892", number: "5", name: "Movimientos", icon:  <MdShoppingCartCheckout /> },
-    { bgColor: "#FFC39D", number: "6", name: "Reportes", icon: <TbReportAnalytics />    }
+    { bgColor: "#00CED1 ", number: "1", name: "Facturas", icon: <FaFileInvoiceDollar /> },
+    { bgColor: "#FF6F61 ", number: "2", name: "Inventario", icon: <MdOutlineInventory /> },
+    { bgColor: "#00CED1 ", number: "4", name: "Clientes", icon: <FaUsersViewfinder /> },
+    { bgColor: "#FF6F61 ", number: "3", name: "Vehiculos", icon: <PiMotorcycleFill /> },
+    { bgColor: "#00CED1", number: "5", name: "Movimientos", icon:  <MdShoppingCartCheckout /> },
+    { bgColor: "#FF6F61 ", number: "6", name: "Reportes", icon: <TbReportAnalytics />    }
   ];
 
   return (
@@ -22,10 +24,17 @@ const dashboard = () => {
         <h1 className='text-left font-bold text-gray-600'>ELECTRIMOTOS ZAPATOCA</h1>
         <DateTimeDisplay className="flex justify-between"  />
       </div>
-      
-      <div className="grid grid-cols-3 grid-rows-2 gap-4 p-5 m-4 h-full bg-white rounded-sm shadow-xl mt-0 border-t-3 border-green-500">
+      <div className="grid grid-cols-3 grid-rows-2 gap-4 p-5 m-4 h-full bg-white rounded-sm shadow-xl mt-0 border-t-3 border-[#FFD700]">
         {blocks.map((block, index) => (
-          <div key={index} className={`bg-[${block.bgColor}] flex flex-row items-center justify-center`}>
+
+          <div 
+            key={index} 
+            style={{ backgroundColor: block.bgColor }} 
+            className={"flex flex-row items-center justify-center"}
+            onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = "#FFA500")}
+            onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = block.bgColor)}
+            onClick={() => navigate("/look")} 
+          > {/* cambiar /look por la ruta corespondinte*/}
             {/* Primera mitad */}
             <div className='w-full flex flex-col items-center justify-center'>
               {/*<div className='text-5xl items-center justify-center'>{block.number}</div>*/}
